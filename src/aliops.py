f1a1dddfd33f85efda4cb95ef6c790be15bcabd6
UnknowName <burtte@sina.com>
2020-03-08 12:59:00 +0800

    update

--- src/aliops.py
+++ src/aliops.py
@@ -41,7 +41,7 @@ async def _get_status(site: str, host: str):
             async with session.get(url, timeout=5) as resp:
                 return resp.status, host
     except Exception as e:
-        print(e)
+        print("host {host} occur error is {err}".format(host=host, err=e))
         return 504, host
 
 
@@ -51,22 +51,20 @@ async def check(request):
         O2O3='172.18.17.73', O2O4='172.18.17.70', O2O5='172.18.17.72', O2O6='172.18.17.74', O2O8='172.18.17.69',
         O2O9='172.18.17.68', O2O10='172.18.17.64', O2O11='172.18.17.67', O2O12='172.18.17.60', O2O13='172.18.17.66',
         O2O14='172.18.17.63', O2O15='172.18.17.79', O2O16='172.18.17.78', O2O17='172.18.0.205', O2O18='172.18.0.204',
-        O2O19='172.18.0.206', O2O20='172.18.208', O2O21='172.18.0.213', O2O22='172.18.0.212', O2O23='172.18.0.217',
+        O2O19='172.18.0.206', O2O20='172.18.0.208', O2O21='172.18.0.213', O2O22='172.18.0.212', O2O23='172.18.0.217',
         O2O24='172.18.0.216', O2O25='172.18.203.241', O2O26='172.18.203.243', O2O27='172.18.203.244'
     )
-    tasks = [
-        _get_status(domain, host) for host in servers.values()
-    ]
+    tasks = [_get_status(domain, host) for host in servers.values()]
     dones, _ = await asyncio.wait(tasks, timeout=6)
     all_results = [done.result() for done in dones]
     response = ""
     ok = 0
     for status, host in all_results:
-        if status == 200:
+        if status in [200, 301, 302]:
             ok += 1
         response += "{host}  {status}\n".format(host=host, status=status)
     rate = ok / len(all_results) * 100
-    response += 'Rate: {}%'.format(rate)
+    response += '\nRate: {}%'.format(rate)
     return web.Response(status=200, text=response)
 
 
